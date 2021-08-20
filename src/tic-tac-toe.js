79a3fb70ac541b0c652853d569c24f114bc86260
Vitali Hauzinski <hauzinski@gmail.com>
2021-08-20 20:29:32 +0300

    feat: add all functions
    fix: fix errors

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -6,7 +6,8 @@ class TicTacToe {
       [null, null, null],
     ];
     this.equal = "x";
-    this.winer = null;
+    this.winner = null;
+    this.gameOver = false;
   }
 
   getCurrentPlayerSymbol() {
@@ -14,9 +15,34 @@ class TicTacToe {
   }
 
   nextTurn(rowIndex, columnIndex) {
-    if (rowIndex >= 0 && rowIndex < 3 && columnIndex >= 0 && columnIndex < 3) {
+    if (!this.gameOver) {
       if (this.arr[rowIndex][columnIndex] === null) {
         this.arr[rowIndex].splice(columnIndex, 1, this.equal);
+
+        if (this.arr[0][0] === this.arr[1][1] && this.arr[1][1] === this.arr[2][2] && !!this.arr[0][0] && !!this.arr[1][1] && !!this.arr[2][2]) {
+          this.gameOver = true;
+          this.winner = this.equal;
+        }
+
+        if (this.arr[0][2] === this.arr[1][1] && this.arr[1][1] === this.arr[2][0] && !!this.arr[0][2] && !!this.arr[1][1] && !!this.arr[2][0]) {
+          this.gameOver = true;
+          this.winner = this.equal;
+        }
+
+        for (let i = 0; i < 3; i++) {
+          if (this.arr[i][0] === this.arr[i][1] && this.arr[i][1] === this.arr[i][2] && !!this.arr[i][0] && !!this.arr[i][1] && !!this.arr[i][2]) {
+            this.gameOver = true;
+            this.winner = this.equal;
+          }
+        }
+
+        for (let i = 0; i < 3; i++) {
+          if (this.arr[0][i] === this.arr[1][i] && this.arr[1][i] === this.arr[2][i] && !!this.arr[0][i] && !!this.arr[1][i] && !!this.arr[2][i]) {
+            this.gameOver = true;
+            this.winner = this.equal;
+          }
+        }
+
         if (this.equal === "x") {
           this.equal = "o";
         } else {
@@ -26,20 +52,21 @@ class TicTacToe {
     }
   }
 
-  isFinished() {}
-
-  getWinner() {}
-
-  noMoreTurns() {
-    for (let value of this.arr) {
-      if (value.includes(null, 0)) {
-        return false;
-      }
-    }
-    return true;
+  isFinished() {
+    return this.winner !== null || this.arr.flat().includes(null, 0) === false;
   }
 
-  isDraw() {}
+  getWinner() {
+    return this.winner;
+  }
+
+  noMoreTurns() {
+    return this.arr.flat().includes(null, 0) ? false : true;
+  }
+
+  isDraw() {
+    return this.winner === null && this.arr.flat().includes(null, 0) === false;
+  }
 
   getFieldValue(rowIndex, colIndex) {
     return this.arr[rowIndex][colIndex];
