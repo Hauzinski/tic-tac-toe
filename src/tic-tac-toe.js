e41201bac44cd1fb71bbd7740b97714bdf7b7ef5
Vitali Hauzinski <hauzinski@gmail.com>
2021-08-20 17:41:47 +0300

    init: start tic-tac-toe
    feat: add functions  getCurrentPlayerSymbol, nextTurn, noMoreTurns,  getFieldValue.

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,49 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.arr = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    this.equal = "x";
+    this.winer = null;
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.equal;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (rowIndex >= 0 && rowIndex < 3 && columnIndex >= 0 && columnIndex < 3) {
+      if (this.arr[rowIndex][columnIndex] === null) {
+        this.arr[rowIndex].splice(columnIndex, 1, this.equal);
+        if (this.equal === "x") {
+          this.equal = "o";
+        } else {
+          this.equal = "x";
+        }
+      }
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {}
 
+  getWinner() {}
+
+  noMoreTurns() {
+    for (let value of this.arr) {
+      if (value.includes(null, 0)) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  isDraw() {}
 
-    }
-
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.arr[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
